b639378b5b7f995cc4c76256f8b2fb1f403749af
fidannh <fidannh@code.edu.az>
2024-04-02 19:14:09 +0400

    Done

--- dragTask/js/index.js
+++ dragTask/js/index.js
@@ -1,37 +1,61 @@
 
+
 let doneList = document.getElementById("doneTaskList");
-let todoList = document.getElementById("todoTaskList");
 let listItems = document.querySelectorAll(".task-list li");
 let dropAreas = document.querySelectorAll(".drop-area");
 
 listItems.forEach((elem, index) => {
     elem.addEventListener("dragstart", function (e) {
-        e.dataTransfer.setData("index", index);
-    });
-});
+        e.dataTransfer.setData("index", index)
+    })
+})
 
 dropAreas.forEach(elem => {
     elem.addEventListener("dragover", function (e) {
         e.preventDefault();
-    });
+    })
+})
 
+dropAreas.forEach(elem => {
     elem.addEventListener("drop", function (e) {
         let index = e.dataTransfer.getData("index");
-        elem.querySelector("ul").appendChild(listItems[index]);
-        listItems[index].style.textDecoration = "line-through";
-    });
-});
+        elem.querySelector("ul").appendChild(listItems[index])
+    })
+})
 
-todoList.addEventListener("dragover", function (e) {
-    e.preventDefault();
-});
+// let doneList = document.getElementById("doneTaskList");
+// let todoList = document.getElementById("todoTaskList");
+// let listItems = document.querySelectorAll(".task-list li");
+// let dropAreas = document.querySelectorAll(".drop-area");
 
-todoList.addEventListener("drop", function (e) {
-    let index = e.dataTransfer.getData("index");
-    todoList.appendChild(listItems[index]);
-    listItems[index].style.textDecoration = "none";
+// listItems.forEach((elem, index) => {
+//     elem.addEventListener("dragstart", function (e) {
+//         e.dataTransfer.setData("index", index);
+//     });
+// });
+
+// dropAreas.forEach(elem => {
+//     elem.addEventListener("dragover", function (e) {
+//         e.preventDefault();
+//     });
+
+//     elem.addEventListener("drop", function (e) {
+//         let index = e.dataTransfer.getData("index");
+//         elem.querySelector("ul").appendChild(listItems[index]);
+//         listItems[index].style.textDecoration = "line-through";
+//     });
+// });
+
+// todoList.addEventListener("dragover", function (e) {
+//     e.preventDefault();
+// });
+
+// todoList.addEventListener("drop", function (e) {
+//     let index = e.dataTransfer.getData("index");
+//     todoList.appendChild(listItems[index]);
+//     listItems[index].style.textDecoration = "none";
     //  listItems[index].style.removeProperty("text-decoration");
-});
+// });
 
 // todoList.addEventListener("drop", function (e) {
 //     let index = e.dataTransfer.getData("index");
@@ -50,7 +74,6 @@ todoList.addEventListener("drop", function (e) {
 // });
 
 // fileInput.addEventListener('change', function() {
-//     // Handle file selection if needed
 // });
 
 
